3d522541b6d7058d6f45d21bcec6bf32487f4497
YuRaNnNzZZ <medved609@yandex.ru>
2019-10-17 09:42:28 +0300

    re-enabled CraftTweaker plugin

--- src/main/java/joshie/harvest/plugins/crafttweaker/base/BaseOnce.java
+++ src/main/java/joshie/harvest/plugins/crafttweaker/base/BaseOnce.java
@@ -1,28 +1,28 @@
-//package joshie.harvest.plugins.crafttweaker.base;
-//
-//import crafttweaker.IAction;
-//
-//public abstract class BaseOnce implements IAction {
-//    private boolean applied;
-//
-//    public abstract String getDescription();
-//
-//    @Override
-//    public void apply() {
-//        if (!isApplied()) {
-//            applied = true;
-//            applyOnce();
-//        }
-//    }
-//
-//    public boolean isApplied() {
-//        return applied;
-//    }
-//
-//    public abstract void applyOnce();
-//
-//    @Override
-//    public String describe() {
-//        return "[Harvest Festival] " + getDescription();
-//    }
-//}
+package joshie.harvest.plugins.crafttweaker.base;
+
+import crafttweaker.IAction;
+
+public abstract class BaseOnce implements IAction {
+    private boolean applied;
+
+    public abstract String getDescription();
+
+    @Override
+    public void apply() {
+        if (!isApplied()) {
+            applied = true;
+            applyOnce();
+        }
+    }
+
+    public boolean isApplied() {
+        return applied;
+    }
+
+    public abstract void applyOnce();
+
+    @Override
+    public String describe() {
+        return "[Harvest Festival] " + getDescription();
+    }
+}
--- src/main/java/joshie/harvest/plugins/crafttweaker/command/HFCommandShops.java
+++ src/main/java/joshie/harvest/plugins/crafttweaker/command/HFCommandShops.java
@@ -1,50 +1,50 @@
-//package joshie.harvest.plugins.crafttweaker.command;
-//
-//import joshie.harvest.api.shops.Shop;
-//import joshie.harvest.core.commands.CommandManager.CommandLevel;
-//import net.minecraft.command.CommandBase;
-//import net.minecraft.command.CommandException;
-//import net.minecraft.command.ICommandSender;
-//import net.minecraft.server.MinecraftServer;
-//import net.minecraft.util.ResourceLocation;
-//import net.minecraft.util.text.TextComponentString;
-//
-//import javax.annotation.Nonnull;
-//
-//import crafttweaker.CraftTweakerAPI;
-//
-//import java.util.Comparator;
-//import java.util.List;
-//import java.util.Map.Entry;
-//import java.util.stream.Collectors;
-//
-//public class HFCommandShops extends CommandBase {
-//    @Override
-//    @Nonnull
-//    public String getName() {
-//        return "shops";
-//    }
-//
-//    @Override
-//    @Nonnull
-//    public String getUsage(@Nonnull ICommandSender sender) {
-//        return "/hf shops";
-//    }
-//
-//    @Override
-//    public int getRequiredPermissionLevel() {
-//        return CommandLevel.ANYONE.ordinal();
-//    }
-//
-//    @Override
-//    public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] parameters) throws CommandException {
-//        CraftTweakerAPI.logCommand("Shops: \n" + this.getShopList().toString().replace("[", "").replace("]", "").replace(", ", "\n"));
-//        sender.sendMessage(new TextComponentString("List generated; see minetweaker.log in your minecraft dir"));
-//    }
-//
-//    private List<ResourceLocation> getShopList() {
-//        List<ResourceLocation> list = Shop.REGISTRY.entrySet().stream().map(Entry::getKey).collect(Collectors.toList());
-//        list.sort(Comparator.comparing(ResourceLocation::toString));
-//        return list;
-//    }
-//}
+package joshie.harvest.plugins.crafttweaker.command;
+
+import joshie.harvest.api.shops.Shop;
+import joshie.harvest.core.commands.CommandManager.CommandLevel;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextComponentString;
+
+import javax.annotation.Nonnull;
+
+import crafttweaker.CraftTweakerAPI;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+public class HFCommandShops extends CommandBase {
+    @Override
+    @Nonnull
+    public String getName() {
+        return "shops";
+    }
+
+    @Override
+    @Nonnull
+    public String getUsage(@Nonnull ICommandSender sender) {
+        return "/hf shops";
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return CommandLevel.ANYONE.ordinal();
+    }
+
+    @Override
+    public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] parameters) throws CommandException {
+        CraftTweakerAPI.logCommand("Shops: \n" + this.getShopList().toString().replace("[", "").replace("]", "").replace(", ", "\n"));
+        sender.sendMessage(new TextComponentString("List generated; see minetweaker.log in your minecraft dir"));
+    }
+
+    private List<ResourceLocation> getShopList() {
+        List<ResourceLocation> list = Shop.REGISTRY.entrySet().stream().map(Entry::getKey).collect(Collectors.toList());
+        list.sort(Comparator.comparing(ResourceLocation::toString));
+        return list;
+    }
+}
